b5d5682de43aaac196cc3cd7ce93da278e640591
Tulio Moura <tuliofmoura@gmail.com>
2022-03-17 10:25:34 -0300

    new project creation

---
+++ hello_dart/dart_console_app/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_console_app/lib.dart';
+
+void main(List<String> arguments) {
+  print(arguments);
+}
---
+++ quiz_example/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Text("Hello!"),
+    );
+  }
+}
